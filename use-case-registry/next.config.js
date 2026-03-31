23c19d82418cc7c897cb69c63e5ba3218535c1bb
Brandon Dorman <brandon.dorman@gmail.com>
2026-03-31 06:47:20 -0700

    append

--- use-case-registry/next.config.js
+++ use-case-registry/next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : '',
-  images: { unoptimized: true },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
